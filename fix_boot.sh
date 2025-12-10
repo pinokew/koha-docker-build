fd4a41b2a6a3c3ecaaa23e81d5ef0a70f82f126a
pinokew <cestlawie235@gmail.com>
2025-12-10 12:35:50 +0200

    autoboot fixed

---
+++ fix_boot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Чекаємо 15 секунд, щоб мережа та DNS точно піднялися
+sleep 15
+
+cd $HOME/Koha/koha-doker
+
+# Примусовий перезапуск для чистого старту
+docker compose down
+docker compose up -d
