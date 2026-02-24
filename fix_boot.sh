4c85ccce4d473dae2cbf9978b6d21dd03c0ab805
pinokew <cestlawie235@gmail.com>
2026-02-24 21:34:57 +0200

    koha

--- fix_boot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Чекаємо секунд, щоб мережа та DNS точно піднялися
-sleep 120
-
-cd $HOME/Koha/koha-doker
-
-# Примусовий перезапуск для чистого старту
-docker compose down
-docker compose up -d
